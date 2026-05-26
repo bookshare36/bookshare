4a231a66cb01b05d70dce89bdfaa55f4b8d7e882
bookshare36 <bookshare36@gmail.com>
2026-05-26 11:07:21 +0200

    Update posts.js

--- functions/posts.js
+++ functions/posts.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: false, sslmode: 'verify-full' }
 });
 
 exports.handler = async (event) => {
